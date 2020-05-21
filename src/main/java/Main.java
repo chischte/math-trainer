490e9f57eed2aba498a9133fcb18db5d91259e5f
Michael Wettstein <chischte@gmx.ch>
2020-05-21 18:40:54 +0200

    Write main loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,30 @@
 package main.java;
 
-public class Main {
-    public static void main(String args){
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+        boolean stayInLoop = true;
+        Scanner scanner = new Scanner(System.in);
+
+        // GET INITIAL QUESTION
+        System.out.println("1+1=");
+
+        while (true) {
+
+            // GET USER RESPONSE
+            String name = scanner.nextLine();
+            int userAnswer = Integer.parseInt(name);
+
+            // CHECK IF ANSWER IS CORRECT
+            if (userAnswer == 2) {
+                System.out.println("CORRECT!");
+                //GET NEW QUESTION
+            }
+            else{
+                System.out.println("WRONG!");
+            }
+        }
     }
 }
