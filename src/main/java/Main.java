09dfe09e1c0b08c0b71418ae8b5f49580bab29ae
Michael Wettstein <chischte@gmx.ch>
2020-05-22 11:05:05 +0200

    Add try catch to catch number format exception

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ package main.java;
 import java.util.Scanner;
 
 // TODO: Make sound when correct
-// TODO: Fix bug programm aborts when text entry or no entry
 // TODO: Stop time, calculate speed in calcs/minute
 
 public class Main {
@@ -11,18 +10,24 @@ public class Main {
     public static void main(String[] args) {
         boolean stayInLoop = true;
         Scanner scanner = new Scanner(System.in);
-        QuestionGenerator generator=new QuestionGenerator();
+        QuestionGenerator generator = new QuestionGenerator();
 
         // GET INITIAL QUESTION
         generator.generateNewQuestion();
-
 
         while (true) {
             System.out.println(generator.getQuestionstring());
 
             // GET USER RESPONSE
             String userInput = scanner.nextLine();
-            int userAnswer = Integer.parseInt(userInput);
+
+            int userAnswer= 0;
+
+            try {
+                userAnswer = Integer.parseInt(userInput);
+            } catch (NumberFormatException nfe) {
+                System.out.println("Invalid entry: " + nfe.getMessage());
+            }
 
 
             // CHECK IF ANSWER IS CORRECT
@@ -30,9 +35,8 @@ public class Main {
                 System.out.println("CORRECT!");
                 generator.generateNewQuestion();
                 generator.countSolvedQuestionOneUp();
-                System.out.println("Number of solved questions: " +generator.getSolvedQuestions());
-            }
-            else{
+                System.out.println("Number of solved questions: " + generator.getSolvedQuestions());
+            } else {
                 System.out.println("WRONG!");
             }
         }
